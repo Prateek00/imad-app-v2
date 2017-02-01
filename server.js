fbf567ecf2384e68c07aee5bd5468040d4cbe9f9
Prateek00 <ced14i028@iiitdm.ac.in>
2017-02-01 16:17:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/atricle_1',function(req,res){
+ res.send('Article one will be served here');    
+});
+
+app.get('/atricle_2',function(req,res){
+ res.send('Article two will be served here');    
+});
+
+app.get('/atricle_3',function(req,res){
+ res.send('Article three will be served here');    
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
