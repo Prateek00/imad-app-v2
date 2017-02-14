6391c5f2255de8c783fdbbe6bd449afa4413be2f
Prateek Verma <ced14i028@iiitdm.ac.in>
2017-02-15 02:25:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,6 +22,13 @@ app.get('/atricle_3',function(req,res){
  res.send('Article three will be served here');    
 });
 
+var counter=0;
+app.get('/counter',function(req,res){
+   counter+=1;
+   res.send(counter.toString());
+    
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
