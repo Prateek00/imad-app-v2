5f996e04c01c24f7ae867786f3ac54f73e838bb4
Prateek Verma <ced14i028@iiitdm.ac.in>
2017-02-15 03:02:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var button = document.getElementById('counter');
 
 var counter = 0;
-button.onclick = fumction(){
+button.onclick = function(){
     
     counter = counter + 1;
     var span = document.getElementById('count');
